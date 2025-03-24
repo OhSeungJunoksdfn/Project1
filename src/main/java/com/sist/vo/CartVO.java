27a5035424745f24073ad73d309c7613ecb81231
sist-106 <sist-106@DESKTOP-RE7C273>
2025-03-25 08:44:26 +0900

    Jjim VO / CartVO 2025-03-25 전현빈 생성 및 cpvo JOIN

--- src/main/java/com/sist/vo/CartVO.java
+++ src/main/java/com/sist/vo/CartVO.java
@@ -1,5 +1,13 @@
 package com.sist.vo;
 
-public class CartVO {
+import java.util.*;
 
+import lombok.Data;
+
+@Data
+public class CartVO {
+	private int cno, pno, account;
+	private String id;
+	private Date regdate;
+	private Cocktail_ProductVO cpvo=new Cocktail_ProductVO();
 }
